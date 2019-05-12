e49b63f9a89fe429753ee55c5fdacea4276eefc7
yogithesymbian <yogirenbox33@gmail.com>
2019-05-12 09:56:56 -0400

    chapter2

---
+++ python-grayhat/findinstruction.py
@@ -0,0 +1,2 @@
+# Finding Exploit-Friendly Instructions
+from immlib import *
---
+++ python-grayhat/structure.py
@@ -0,0 +1,11 @@
+# class food_recipe(Structure):
+#     _field_ = [
+#         ('egg', c_int),
+#         ('rice', c_int),
+#     ]
+
+counter = 0
+
+while counter < 10:
+    print "Loop number : %d " % counter
+    counter += 1
